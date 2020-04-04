b116f172e9559af3d1ba4dabbec696eb6ac336de
Andrew Baird <baird.andrew4@gmail.com>
2020-04-04 16:09:28 -0500

    Getting bearings in the index.html and index.js files

--- flashcardfun/src/index.js
+++ flashcardfun/src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
+//import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './App.css';
 import * as serviceWorker from './serviceWorker';
+import { render } from '@testing-library/react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function CardsinDeck(props){
+  return(
+    <span>
+      <h1>{props.cardTitle}</h1>
+    </span>
+  )
+}
+
+export default CardsinDeck;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
