1eac3702e8e73de9eeb1a5cb971dc7ee87e21074
Andrew Baird <baird.andrew4@gmail.com>
2020-04-15 09:09:47 -0500

    Taking a fresh approach of how to start after further research

--- flashcardfun/src/App.js
+++ flashcardfun/src/App.js
@@ -1,28 +1,48 @@
-import React from 'react';
-import CardsAPI from './FlashCardAPI'
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CardsAPI from './FlashCardAPI';
 import './App.css';
-import { render } from '@testing-library/react';
 
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CollectionList = (props) => (
+	<div>
+		{props.collections.map((collection) => (
+			<Collection key={collection.id} {...collection} />
+		))}
+	</div>
+);
+
+class Collection extends Component {
+	render() {
+		const collection = this.props;
+		return (
+			<div className="collection">
+				<div className="info">
+					<div className="name">{collection.title}</div>
+					<div className="cards">{collection.cards}</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			collections: [],
+		};
+	}
+	addNewCollection = (collectionData) => {
+		this.setState((prevState) => ({
+			collections: [...prevState.collections, collectionData],
+		}));
+	};
+	render() {
+		return (
+			<div>
+				<div className="header">{this.props.title}</div>
+				<CollectionList cards={this.state.cards} />
+			</div>
+		);
+	}
 }
 export default App;
-*/
